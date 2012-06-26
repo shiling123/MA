2e171e44fa95a405d4f9ef7df42e190a52afa003
shiling123 <Ling.Shi@thomsonreuters.com>
2012-06-26 11:55:10 +0800

    daily

---
+++ src/com/ling/algorithms/StackRelated.java
@@ -0,0 +1,52 @@
+package com.ling.algorithms;
+
+import java.util.Stack;
+
+public class StackRelated {
+
+	/**
+	 *设计包含min 函数的栈。 
+    定义栈的数据结构，要求添加一个min 函数，能够得到栈的最小元素。 
+    要求函数min、push 以及pop 的时间复杂度都是O(1)。 
+	 */
+	public static void main(String[] args) {
+		MinStack<Float> a = new MinStack<Float>();
+		a.push(1.0f);		System.out.println("min(): " + a.min()); 
+		a.push(2.0f);		System.out.println("min(): " + a.min()); 
+		a.push(4.2f);		System.out.println("min(): " + a.min()); 
+		a.push(0.5f);		System.out.println("min(): " + a.min()); 		
+		
+		MinStack<Integer> b = new MinStack<Integer>();
+		b.push(1);		System.out.println("min(): " + b.min()); 
+		b.push(2);		System.out.println("min(): " + b.min()); 
+		b.push(4);		System.out.println("min(): " + b.min()); 
+		b.push(0);		System.out.println("min(): " + b.min()); 	
+	}
+	
+	
+	public static class MinStack<E extends Comparable< ? super E>> {
+		private Stack<E> items = new Stack<E>();
+		private Stack<E> mins = new Stack<E>();
+
+		public void push (E i) {
+			if(items.size() == 0) {
+				mins.push(i);
+			} else {				
+				E m = mins.peek();
+				E min =i.compareTo(m)> 0? m:i;
+				mins.push(min);
+			}
+			items.push(i);
+		}
+
+		public E pop() {
+			mins.pop();
+			return items.pop();
+		}
+		
+		public E min() {
+			return mins.peek();			
+		}
+	}
+
+}
