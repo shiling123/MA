2cc06121a5e54136b9b9d8f879ea3f1e5bde1994
shiling123 <Ling.Shi@thomsonreuters.com>
2012-06-27 14:49:40 +0800

    daily

--- src/com/ling/algorithms/StackRelated.java
+++ src/com/ling/algorithms/StackRelated.java
@@ -10,17 +10,20 @@ public class StackRelated {
     要求函数min、push 以及pop 的时间复杂度都是O(1)。 
 	 */
 	public static void main(String[] args) {
-		MinStack<Float> a = new MinStack<Float>();
-		a.push(1.0f);		System.out.println("min(): " + a.min()); 
-		a.push(2.0f);		System.out.println("min(): " + a.min()); 
-		a.push(4.2f);		System.out.println("min(): " + a.min()); 
-		a.push(0.5f);		System.out.println("min(): " + a.min()); 		
-		
-		MinStack<Integer> b = new MinStack<Integer>();
-		b.push(1);		System.out.println("min(): " + b.min()); 
-		b.push(2);		System.out.println("min(): " + b.min()); 
-		b.push(4);		System.out.println("min(): " + b.min()); 
-		b.push(0);		System.out.println("min(): " + b.min()); 	
+//		MinStack<Float> a = new MinStack<Float>();
+//		a.push(1.0f);		System.out.println("min(): " + a.min()); 
+//		a.push(2.0f);		System.out.println("min(): " + a.min()); 
+//		a.push(4.2f);		System.out.println("min(): " + a.min()); 
+//		a.push(0.5f);		System.out.println("min(): " + a.min()); 		
+//		
+//		MinStack<Integer> b = new MinStack<Integer>();
+//		b.push(1);		System.out.println("min(): " + b.min()); 
+//		b.push(2);		System.out.println("min(): " + b.min()); 
+//		b.push(4);		System.out.println("min(): " + b.min()); 
+//		b.push(0);		System.out.println("min(): " + b.min()); 
+		int[] a = {1,2,3,4};
+		int[] b= {1,3,2,4};
+		System.out.println(checkOrder(a,b));
 	}
 	
 	
@@ -65,6 +68,31 @@ public class StackRelated {
 			}
 			return b.pop();
 		}
+	}	
+	
+	public static boolean checkOrder(int[] push,int [] pop) {
+		if(push.length != pop.length) {
+			return false;
+		}		
+		int len = push.length;
+		Stack<Integer> stack = new Stack<Integer>();
+		int i = 0;
+		int j = 0;
+		while(i<len) {
+			int ori = pop[i];
+			while(stack.isEmpty()||stack.peek()!=ori) {
+				if(j<len) {
+					stack.push(push[j]);
+					j++;
+				} else {
+					return false;
+				}
+			}
+			while(!stack.isEmpty()&&stack.peek()==ori){
+				stack.pop();
+				i ++;
+			}		
+		}		
+		return true;
 	}
-
 }
