bc514a222ad7be1f82c848bda594761ea1edf54b
shiling123 <Ling.Shi@thomsonreuters.com>
2012-06-29 11:24:57 +0800

    daily

--- src/com/ling/algorithms/tree.cpp
+++ src/com/ling/algorithms/tree.cpp
@@ -26,3 +26,24 @@ void visitBylevel(Node * root )
 	}
     }
 }
+
+Node * findLowestParent(Node* root, Node * child1, Node * child2) {
+  if( root == NULL ){
+    return NULL;
+  }
+
+  if(root == child1 || root == child2 ) {
+    return root;
+  }
+
+  Node * left = findLowestParent(root->left,child1,child2);
+  Node * right = findLowestParent(root->right, child1,child2);
+
+  if( left == NULL ){
+    return rigth;
+  } else if( right == NULL ) {
+    return left;
+  } else {
+    return root;
+  }
+}
